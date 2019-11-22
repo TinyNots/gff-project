b5541dba719299e60b5abd48c21faa03bcaf4f64
白枝友紀 <saokurokirito4624@i.softbank.jp>
2019-11-22 18:30:42 +0900

    タイトルシーン,ゲームシーン,リザルトシーンを追加しました。 タイトルシーンでは別のシーンに飛べるよう、ボタンの選択機能を追加しました。

---
+++ Global/Assets/Scripts/SceneButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneButton : MonoBehaviour
+{
+    [SerializeField]
+    private SceneCtl.SCENE_ID _sceneID;
+    public void ChangeScene()
+    {
+        if(SceneCtl.instance != null)
+        {
+            SceneCtl.instance.NextScene(_sceneID);
+        }
+    }
+}
---
+++ Global/Assets/Scripts/StartButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    [SerializeField, Tooltip("無効化するオブジェクト")]
+    GameObject[] _noActiveObj;
+    [SerializeField]
+    GameObject[] Buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PushButton()
+    {
+        foreach(var button in Buttons)
+        {
+            button.SetActive(true);
+        }
+        foreach (var obj in _noActiveObj)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonUp("Fire1") || Input.GetKeyUp(KeyCode.A))
+        {
+            PushButton();
+        }
+    }
+}
