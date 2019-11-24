3840fa249750a9d0d02016ee45ef0dcff52672c5
Tan Soon Seang <alextan1751@gmail.com>
2019-11-25 00:15:13 +0900

    複数のコントローラへの対応を完成しました。

---
+++ Global/Assets/Scripts/GamePadManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePadManager : MonoBehaviour
+{
+    [SerializeField]
+    private int _gamepadCount = 2;
+
+    private List<Controller> _gamepads;
+    private static GamePadManager _instance;
+
+    void Awake()
+    {
+        _instance = this;
+        if (_instance == null && _instance != this)
+        {
+            //Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(_instance);
+
+        _gamepadCount = Mathf.Clamp(_gamepadCount, 1, 4);
+
+        _gamepads = new List<Controller>();
+
+        for (int i = 0; i < _gamepadCount; i++)
+        {
+            _gamepads.Add(new Controller(i));
+        }
+    }
+
+    void Update()
+    {
+        Refresh();
+
+        foreach (Controller gamepad in _gamepads)
+        {
+            gamepad.Update();
+        }
+    }
+
+    public static GamePadManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.LogError("[GamepadManager]: Instance does not exist!");
+                return null;
+            }
+            return _instance;
+        }
+    }
+
+    public void Refresh()
+    {
+        foreach(Controller gamePad in _gamepads)
+        {
+            gamePad.Refresh();
+        }
+    }
+
+    public Controller GetGamepad(int index)
+    {
+        for (int i = 0; i < _gamepads.Count;)
+        {
+            if (_gamepads[i].Index == (index - 1))
+            {
+                return _gamepads[i];
+            }
+            else
+            {
+                i++;
+                continue;
+            }
+        }
+
+        Debug.LogError("[GamepadManager]: " + index + " is not a valid gamepad index!");
+        return null;
+    }
+
+    public int ConnectedTotal()
+    {
+        int total = 0;
+
+        for (int i = 0; i < _gamepads.Count; i++)
+        {
+            if (_gamepads[i].IsConnected)
+            {
+                total++;
+            }
+        }
+
+        return total;
+    }
+
+    public bool GetButtonAny(string button)
+    {
+        for (int i = 0; i < _gamepads.Count; ++i)
+        {
+            if (_gamepads[i].IsConnected && _gamepads[i].GetButton(button))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool GetButtonDownAny(string button)
+    {
+        for (int i = 0; i < _gamepads.Count; ++i)
+        {
+            if (_gamepads[i].IsConnected && _gamepads[i].GetButtonDown(button))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Global/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Controller gamepad; // Gamepad instance
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Obtain the desired gamepad from GamepadManager
+        gamepad = GamePadManager.Instance.GetGamepad(1);
+
+        // Sample code to test button input and rumble
+        if (gamepad.GetButtonDown("A"))
+        {
+            TestRumble();
+            Debug.Log("A down");
+        }
+
+        transform.Translate(Vector2.right * gamepad.GetStickL().X * Time.deltaTime * 5f);
+    }
+
+    // Send some rumble events to the gamepad
+    void TestRumble()
+    {
+        gamepad.AddRumble(0.5f, 0.1f, new Vector2(1f, 1f));
+        //gamepad.AddRumble(2.5f, 0.2f, new Vector2(0.5f, 0.5f));
+    }
+}
