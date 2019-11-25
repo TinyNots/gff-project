33dc90a547d98f5bcf3580d91d01fd35085d7858
Tan Soon Seang <alextan1751@gmail.com>
2019-11-26 01:00:38 +0900

    プレイヤーの簡易移動とジャンプ（奥ゆきあり）

---
+++ Global/Assets/Scripts/Jump.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private float _jumpVelocity = 20f;
+    private float _currentVelocity = 0;
+    private bool _isGrounded = true;
+    [SerializeField]
+    private PlayerMovement player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GamePadManager.Instance.GetGamepad(1).GetButtonDown("A") && _isGrounded == true)
+        {
+            _currentVelocity = _jumpVelocity;
+            _isGrounded = false;
+        }
+
+        if(!_isGrounded)
+        {
+            _currentVelocity -= 10f * 0.2f;
+            transform.Translate(Vector2.up * _currentVelocity * Time.deltaTime);
+
+            if(_currentVelocity <= -_jumpVelocity)
+            {
+                transform.position = new Vector2(transform.position.x, player.GetDepth());
+                _currentVelocity = 0;
+                _isGrounded = true;
+                StartCoroutine("JumpWait");
+            }
+        }
+    }
+
+    private IEnumerator JumpWait()
+    {
+        player.EableMove = false;
+
+        yield return new WaitForSeconds(0.1f);
+
+        player.EableMove = true;
+    }
+}
--- Global/Assets/Scripts/PlayerMovement.cs
+++ Global/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Controller gamepad; // Gamepad instance
+    public float depth = 0f;
+    [SerializeField]
+    private float _speed = 5.0f;
+    private bool _isEableMove = true;
 
     // Use this for initialization
     void Start()
@@ -15,6 +19,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float halfScreen = Screen.width / 2f;
+        Debug.DrawLine(new Vector3(-halfScreen, 0), new Vector3(halfScreen, 0), Color.white);
         // Obtain the desired gamepad from GamepadManager
         gamepad = GamePadManager.Instance.GetGamepad(1);
 
@@ -25,13 +31,49 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log("A down");
         }
 
-        transform.Translate(Vector2.right * gamepad.GetStickL().X * Time.deltaTime * 5f);
+        //transform.Translate(Vector2.right * gamepad.GetStickL().X * Time.deltaTime * 5f);
+        if (_isEableMove)
+        {
+            depth += _speed * gamepad.GetStickL().Y * Time.deltaTime;
+        }
+
+        depth = Mathf.Clamp(depth, -4.2f, 0.2f);
+
+        // test jump code
+        if (gamepad.GetButtonDown("A"))
+        {
+            //GetComponent<Rigidbody2D>().AddForce(Vector2.up, ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(_isEableMove)
+        {
+            Vector2 movement = new Vector2(gamepad.GetStickL().X, gamepad.GetStickL().Y) * _speed * Time.deltaTime;
+            transform.Translate(movement);
+
+            Vector2 position = transform.position;
+            position.y = Mathf.Clamp(position.y, -4.2f, 0.2f);
+            transform.position = position;
+        }
     }
 
     // Send some rumble events to the gamepad
     void TestRumble()
     {
-        gamepad.AddRumble(0.5f, 0.1f, new Vector2(1f, 1f));
+        gamepad.AddRumble(0.5f, 0.0f, new Vector2(1f, 1f));
         //gamepad.AddRumble(2.5f, 0.2f, new Vector2(0.5f, 0.5f));
+    }
+
+    public float GetDepth()
+    {
+        return depth;
+    }
+
+    public bool EableMove
+    {
+        get { return _isEableMove; }
+        set { _isEableMove = value; }
     }
 }
