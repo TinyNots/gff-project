3dcbe905c6e91d504b0163f4861d7277094436c2
白枝友紀 <saokurokirito4624@i.softbank.jp>
2019-11-27 19:28:52 +0900

    コミット

---
+++ Global/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance = null;
+    private AudioManager Instance
+    {
+        get
+        {
+            if(instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+            }
+            return instance;
+        }
+    }
+
+    [SerializeField]
+    private AudioSource _audioSource = null;
+    [SerializeField]
+    private AudioClip _audioClip = null;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void PlaySE()
+    {
+        _audioSource.PlayOneShot(_audioClip);
+    }
+
+    private void PlayBGM()
+    {
+
+    }
+
+    IEnumerator Sound()
+    {
+        while(true)
+        {
+            if(_audioSource.isPlaying == false)
+            {
+                _audioSource.PlayOneShot(_audioClip);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Global/Assets/Scripts/FadeSprite.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeSprite : MonoBehaviour
+{
+    enum FADE_TYPE
+    {
+        IN,
+        OUT,
+        FLASH,
+        MAX
+    }
+    [SerializeField]
+    private FADE_TYPE _fadeType;
+    [SerializeField]
+    private float _speed = 1;
+    [SerializeField, Tooltip("ループフラグ")]
+    private bool _loopFlag;
+
+    private Material _mat;
+    // 現在の透明度
+    private float _alpha;
+    private float _nowTime;
+    private const int _flashTime = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _alpha = 1;
+        _mat = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void FadeIn()
+    {
+        _alpha += Time.deltaTime * _speed;
+        if (_alpha > 1)
+        {
+            _alpha = 1;
+            if (_loopFlag)
+            {
+                _fadeType = FADE_TYPE.OUT;
+            }
+        }
+    }
+
+    private void FadeOut()
+    {
+        _alpha -= Time.deltaTime * _speed;
+        if (_alpha < 0)
+        {
+            _alpha = 0;
+            if (_loopFlag)
+            {
+                _fadeType = FADE_TYPE.IN;
+            }
+        }
+    }
+
+    private void Flashing()
+    {
+        _nowTime += Time.deltaTime * _speed;
+        if (_nowTime > _flashTime)
+        {
+            _nowTime = 0;
+            _alpha = _alpha == 1 ? 0 : 1;
+        }
+    }
+
+    public void FadeImage()
+    {
+        switch (_fadeType)
+        {
+            case FADE_TYPE.IN:
+                FadeIn();
+                break;
+            case FADE_TYPE.OUT:
+                FadeOut();
+                break;
+            case FADE_TYPE.FLASH:
+                Flashing();
+                break;
+            case FADE_TYPE.MAX:
+            default:
+                break;
+        }
+    }
+
+    private void SetAlpha()
+    {
+        if(_mat == null)
+        {
+            return;
+        }
+        _mat.color = new Color(_mat.color.r, _mat.color.g, _mat.color.b, _alpha);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeImage();
+        SetAlpha();
+    }
+}
