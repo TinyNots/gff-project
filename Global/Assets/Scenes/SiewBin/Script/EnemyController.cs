953b1f01c1d52dc5205b0a738e8dbf15d32eb275
Siew Bin <tekito@kk.kk>
2019-11-29 01:02:41 +0900

    テストスクリプト

--- Global/Assets/Scenes/SiewBin/Script/EnemyController.cs
+++ Global/Assets/Scenes/SiewBin/Script/EnemyController.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    List<Enemy> enemyFlock;
+
+    public float minVelocity = 5;
+    public float maxVelocity = 20;
+    public float randomness = 1;
+    public int flockSize = 20;
+    public GameObject prefab;
+    public GameObject leader;
+
+    public Vector3 flockCenter;
+    public Vector3 flockVelocity;
+
+    private GameObject[] boids;
     // Start is called before the first frame update
     void Start()
     {
-        
+        boids = new GameObject[flockSize];
+        for (var i = 0; i < flockSize; i++)
+        {
+            Vector3 position = new Vector3(
+                Random.value * GetComponent<Collider>().bounds.size.x,
+                Random.value * GetComponent<Collider>().bounds.size.y,0 ) - GetComponent<Collider>().bounds.extents;
+
+            GameObject boid = Instantiate(prefab, transform.position, transform.rotation) as GameObject;
+            boid.transform.parent = transform;
+            boid.transform.localPosition = position;
+            boid.GetComponent<EnemyFlock>().SetController(gameObject);
+            boids[i] = boid;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        Vector3 theCenter = Vector3.zero;
+        Vector2 theVelocity = Vector3.zero;
+
+        foreach (GameObject boid in boids)
+        {
+            theCenter = theCenter + boid.transform.localPosition;
+            theVelocity = theVelocity + boid.GetComponent<Rigidbody2D>().velocity;
+        }
+
+        flockCenter = theCenter / (flockSize);
+        flockVelocity = theVelocity / (flockSize);
+    }  
 
 
 }
