83525708314841910e61aebe1fff4b434699f6ea
Siew Bin <tekito@kk.kk>
2019-12-02 00:51:01 +0900

    振動足しました

--- Global/Assets/Scenes/SiewBin/Script/State/EnemyGetHit.cs
+++ Global/Assets/Scenes/SiewBin/Script/State/EnemyGetHit.cs
@@ -6,19 +6,34 @@ public class EnemyGetHit : IState<Enemy>
 {
     private float hitTime;
     private float hitLen;
+    private float tiltLen;
     public void Enter(Enemy enemy)
     {
         hitTime = Time.time;
         hitLen = enemy.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.length;
+        tiltLen = 2;
     }
 
     public void Execute(Enemy enemy)
     {
+        switch (tiltLen % 2)
+        {
+            case 0:
+                enemy.transform.position += enemy.transform.TransformDirection(0.1f,0f,0f);
+                tiltLen--;
+                break;
+            case 1:
+                enemy.transform.position -= enemy.transform.TransformDirection(0.1f, 0f,0f);
+                tiltLen--;
+
+                break;
+        }
         if (Time.time > hitTime + hitLen)
         {
             enemy.ChangeState(new EnemyPatrol());
             return;
         }
+       
     }
     public void Exit(Enemy enemy)
     {
