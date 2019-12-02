dd504d0d764b747e90a5344fab21804cd699fda4
Tan Soon Seang <alextan1751@gmail.com>
2019-12-03 02:34:17 +0900

    プレイヤーのコンボ（テストプロジェクト）あとは消す

---
+++ Player/Assets/Scripts/AttackManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackManager : MonoBehaviour
+{
+    private Animator _animator;
+    private bool _eableInput = true;
+
+    [SerializeField]
+    private float _waitTime = 0.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = transform.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_animator == null)
+        {
+            Debug.LogError("Animator is missing.");
+            return;
+        }
+
+        if (_animator.GetBool("Moveable"))
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                _animator.SetTrigger("Attack");
+            }
+        }
+    }
+
+    public IEnumerator StopInput()
+    {
+        _animator.SetBool("Moveable", false);
+        yield return new WaitForSeconds(_waitTime);
+        _animator.SetBool("Moveable", true);
+    }
+}
