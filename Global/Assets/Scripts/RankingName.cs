0e790de88bbe3c5d9846ff7cb6682567dc8f6887
白枝友紀 <saokurokirito4624@i.softbank.jp>
2020-01-08 20:09:11 +0900

    ランキング用のテキストをScriptを追加中

---
+++ Global/Assets/Scripts/RankingName.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingName : MonoBehaviour
+{
+    [SerializeField, Tooltip("ランキングネーム")]
+    private Text[] _texts;
+    private int _num;
+    private Text _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    public void SetRunking(Text[] texts)
+    {
+        _texts = texts;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Global/Assets/Scripts/SelectName.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectName : MonoBehaviour
+{
+    [SerializeField, Tooltip("登録できる名前の長さ")]
+    private int _nameLength;
+    // 表示する名前
+    private Text _text;
+    private Controller _gamePad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<Text>();
+        _text.text = " ";
+        for (int i = 0; i < _nameLength; i++)
+        {
+            _text.text += "- ";
+        }
+        Test();
+    }
+    public void Test()
+    {
+        // スタートボタンを押したか
+        int currentIndex = 1;
+        GamePadManager.Instance.GetGamepad(1).HaveTarget = true;
+        _gamePad = GamePadManager.Instance.GetGamepad(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (_gamePad.GetStickL().Y > 0.5)
+        {
+            Debug.Log("上方向");
+        }
+        else if (_gamePad.GetStickL().Y < -0.5)
+        {
+            Debug.Log("下方向");
+        }
+    }
+}
