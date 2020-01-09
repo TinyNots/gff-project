ba53657ec9f51e37f90e5072e765dbaa54d4b081
白枝 <1701319@st.com>
2020-01-09 15:26:28 +0900

    文字入力システムを追加

--- Global/Assets/Scripts/SelectName.cs
+++ Global/Assets/Scripts/SelectName.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,42 +9,88 @@ public class SelectName : MonoBehaviour
     // 表示する名前
     private Text _text;
     private Controller _gamePad;
-	private List<string> _textList;
-	private int _nowSetNameID;
+    // 名前の全体のリスト
+	private List<string> _nameList = new List<string>();
+    private int _nowSetNameID;
+    // 名前の決定
+    private bool _decision;
+    private char[] _names;
+    private int _nowCharID;
+    private string _textData = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     // Start is called before the first frame update
     void Start()
     {
+        _nowCharID = 0;
+        _names = _textData.ToCharArray();
+
+        for(int i = 0; i < _textData.Length; i++)
+        {
+            Debug.Log(_names[i]);
+        }
+        _nowSetNameID = 0;
         _text = GetComponent<Text>();
-        _text.text = " ";
+        _text.text = "";
         for (int i = 0; i < _nameLength; i++)
         {
-			_textList.Add("- ");
+			_nameList.Add("-");
         }
-		
+        SetText();
         Test();
     }
     public void Test()
     {
-        GamePadManager.Instance.GetGamepad(1).HaveTarget = true;
-        _gamePad = GamePadManager.Instance.GetGamepad(1);
+        //GamePadManager.Instance.GetGamepad(1).HaveTarget = true;
+        //_gamePad = GamePadManager.Instance.GetGamepad(1);
     }
+
+    public void SetText()
+    {
+        string str = "";
+        for(int i = 0; i < _nameLength; i++)
+        {
+            str = str + _nameList[i] + " ";
+        }
+        _text.text = " " + str;
+    }
+
 
 	private void SetName()
 	{
-		// 決定ボタンで次の名前のIDに進む
-		if (_gamePad.GetButtonDown("A"))
-		{
-			_nowSetNameID++;
-		}
-		// Cancelボタンで前の名前のIDに戻る
-		else if (_gamePad.GetButtonDown("B"))
-		{
-			_nowSetNameID--;
-		}
-		// ID超え対策 
-		if (_nowSetNameID > _nameLength - 1)
+        if(_nameList[_nowSetNameID] == "-")
+        {
+            _nameList[_nowSetNameID] = "A";
+        }
+        // デバック
+        Debug.Log(_names[_nowCharID].ToString());
+        _nameList[_nowSetNameID] = _names[_nowCharID].ToString();
+
+        //// 決定ボタンで次の名前のIDに進む
+        //if (_gamePad.GetButtonDown("A"))
+        //{
+        //    _nowSetNameID++;
+        //}
+        //// Cancelボタンで前の名前のIDに戻る
+        //else if (_gamePad.GetButtonDown("B"))
+        //{
+        //    _nowSetNameID--;
+        //}
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            _nowSetNameID++;
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            _nowSetNameID--;
+            _nowCharID = 0;
+        }
+
+        // ID超え対策 
+        if (_nowSetNameID > _nameLength - 1)
 		{
 			_nowSetNameID = _nameLength - 1;
+            
 		}
 		else if(_nowSetNameID < 0)
 		{
@@ -56,19 +101,38 @@ public class SelectName : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 設定されたテキストを表示する
+        SetText();
     }
 
     private void LateUpdate()
     {
-        if (_gamePad.GetStickL().Y > 0.5)
+        //if (_gamePad.GetStickL().Y > 0.5)
+        //{
+        //    Debug.Log("上方向");
+        //}
+        //else if (_gamePad.GetStickL().Y < -0.5)
+        //{
+        //    Debug.Log("下方向");
+        //}
+        SetName();
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-
-            Debug.Log("上方向");
+            _nowCharID++;
+            Debug.Log("上入力");
         }
-        else if (_gamePad.GetStickL().Y < -0.5)
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("下方向");
+            _nowCharID--;
+            Debug.Log("下入力");
+        }
+        if(_nowCharID > 25)
+        {
+            _nowCharID = 0;
+        }
+        else if (_nowCharID < 0)
+        {
+            _nowCharID = 25;
         }
     }
 }
---
+++ Global/Assets/Scripts/TextKerning.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Menuのどこに出すかは自由.
+/// </summary>
+[AddComponentMenu("UI/Effects/TextKerning", 0)]
+public class TextKerning : BaseMeshEffect
+{
+    private const int CharaVertexNum = 4;
+    private const float CharaVertexNumF = 4.0f;
+
+    /// <summary>
+    /// 文字間の距離をどれだけ詰めるか.
+    /// 0 で何もしない状態.
+    /// 1 で100%詰め(全ての文字が重なる).
+    /// </summary>
+    [SerializeField]
+    private float kerningRate = 0.0f;
+
+    private Vector3[] centerList = null;
+
+    protected TextKerning()
+    {
+    }
+
+    public override void ModifyMesh(VertexHelper vh)
+    {
+        if (!IsActive() || kerningRate == 0.0f)
+        {
+            return;
+        }
+
+        int count = vh.currentVertCount;
+
+        // 1文字は4頂点のはずなので、余があるならTextではない？.
+        if ((count % CharaVertexNum) != 0)
+        {
+            return;
+        }
+
+        int centerNum = count / CharaVertexNum;
+
+        if (centerList == null || centerList.Length != centerNum)
+        {
+            centerList = new Vector3[centerNum];
+        }
+
+        // 4頂点毎の座標の重心を計算する.
+        UIVertex vert = new UIVertex();
+        Vector3 vec3 = Vector3.zero;
+
+        for (int i = 0, j = 0, cnt = 0; i < count; ++i)
+        {
+            vh.PopulateUIVertex(ref vert, i);
+
+            vec3 += vert.position;
+
+            ++cnt;
+            if (cnt >= CharaVertexNum)
+            {
+                centerList[j] = vec3 / CharaVertexNumF;
+                ++j;
+                cnt = 0;
+                vec3 = Vector3.zero;
+            }
+        }
+
+        // 文字全体を左寄せする.
+        // 右寄せやら中央寄せが欲しいなら別途処理を作るしかないが、Text のアライメント指定があるので基本不要なはず？.
+        float kerning = 0.0f;
+
+        for (int i = CharaVertexNum, j = 0, cnt = 1; i < count; i += CharaVertexNum)
+        {
+            vec3 = centerList[cnt - 1] - centerList[cnt];
+            ++cnt;
+
+            kerning += vec3.magnitude * kerningRate;
+
+            for (j = 0; j < CharaVertexNum; ++j)
+            {
+                vh.PopulateUIVertex(ref vert, i + j);
+
+                vec3 = vert.position;
+                vec3.x -= kerning;
+                vert.position = vec3;
+
+                vh.SetUIVertex(vert, i + j);
+            }
+        }
+    }
+}
