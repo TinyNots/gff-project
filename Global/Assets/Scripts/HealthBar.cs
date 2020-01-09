da8670a9ea83d70be755a7f025e67d9413ca836a
Siew Bin <tekito@kk.kk>
2020-01-09 23:13:19 +0900

    ＨＰバーを追加しました

--- Global/Assets/Scripts/HealthBar.cs
+++ Global/Assets/Scripts/HealthBar.cs
@@ -6,19 +6,28 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     float maxhp;
-    GameObject health;
+    private GameObject player;
+    Health health;
     public int playerCnt;
     Slider slider;
     // Start is called before the first frame update
     void Start()
     {
-        health = gameObject.Find
-        maxhp = health.HP;
+        slider = GetComponent<Slider>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player == null)
+        {
+            var tmpString = "Player " + playerCnt;
+            player = GameObject.Find(tmpString);
+            health = player.transform.Find("Sprite").GetComponent<Health>();
+            maxhp = health.HP;
+
+        }
         slider.value = health.hp / maxhp;
 
     }
