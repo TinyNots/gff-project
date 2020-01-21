1ba9a82319132c12d371bdac4312fdb188537e15
Tan Soon Seang <alextan1751@gmail.com>
2020-01-21 19:27:46 +0900

    ダッシュの残影追加

---
+++ Global/Assets/Scripts/Ghost.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    [Header("Generals")]
+    [SerializeField]
+    private float _fadeTime;
+    [SerializeField]
+    private Sprite _sprite;
+
+    [Header("Colors")]
+    [SerializeField]
+    private Color _trailColor;
+
+    private SpriteRenderer _sr;
+    private Color _originColor;
+
+    void Start()
+    {
+        _sr = transform.GetComponent<SpriteRenderer>();
+
+        _sr.sprite = _sprite;
+        _sr.color = _trailColor;
+        _originColor = _sr.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_sr.color.a > 0)
+        {
+            _sr.color = new Color(_sr.color.r, _sr.color.g, _sr.color.b, _sr.color.a - (_originColor.a / _fadeTime) * Time.deltaTime);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
