a5cc7d44b85fbc50ba4f1e9c82931e3d9c9a953b
白枝友紀 <saokurokirito4624@i.softbank.jp>
2020-01-22 19:16:31 +0900

    Scene更新

---
+++ Global/Assets/Scripts/StageSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelect : MonoBehaviour
+{
+    // 選択されたSceneに遷移するためのID
+    SceneCtl.SCENE_ID _id;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void SetController()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
